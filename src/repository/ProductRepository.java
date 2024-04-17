8fc37c2e8f856cb5f2591d43c68a7ae23bd26eff
soh1b003 <sohibjinnorboboyev@gmail.com>
2024-04-17 11:58:22 +0500

    user repository sohibdan

--- src/repository/ProductRepository.java
+++ src/repository/ProductRepository.java
@@ -1,4 +1,29 @@
 package repository;
 
-public class ProductRepository {
+import model.Order;
+import model.Product;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.UUID;
+
+public class ProductRepository extends BaseRepository<Product> {
+
+    private static final ProductRepository productRepository = new ProductRepository();
+
+    public static ProductRepository getInstance(){return productRepository;}
+
+    public ProductRepository() {
+    }
+
+    public ArrayList<Product> showCategoryProduct(UUID id){
+        ArrayList<Product> products = new ArrayList<>();
+        for (Product product : data) {
+            if(Objects.equals(product.getCategoryId(), id)){
+                products.add(product);
+            }
+        }
+        return products;
+    }
 }
